2edd7eda665af7ca653134e5b25f31bfa6542be9
SpatLyu <lyu.geosocial@gmail.com>
2026-06-12 00:00:01 +0800

    polish citation

--- R/zzz.R
+++ R/zzz.R
@@ -7,5 +7,5 @@
 .onAttach = function(...){
   packageStartupMessage("This is gdverse ", utils::packageVersion("gdverse"), ".")
   packageStartupMessage("\nTo cite gdverse in publications, please use:
-                         \nLv, W., Lei, Y., Liu, F., Yan, J., Song, Y. and Zhao, W. (2025), gdverse: An R Package for Spatial Stratified Heterogeneity Family. Transactions in GIS, 29(2). https://doi.org/10.1111/tgis.70032")
+                         \nLv, W., Lei, Y., Liu, F., Yan, J., Song, Y., Zhao, W., 2025. gdverse: An R Package for Spatial Stratified Heterogeneity Family. Transactions in GIS 29. https://doi.org/10.1111/tgis.70032")
 }
